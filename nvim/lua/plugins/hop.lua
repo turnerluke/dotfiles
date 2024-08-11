6a0795dfc6871377abc1db93140487873a7a7a66
turner <turnermluke@gmail.com>
2024-08-11 09:51:48 -0500

    Added "hop.nvim"

---
+++ nvim/lua/plugins/hop.lua
@@ -0,0 +1,17 @@
+return {
+	"smoka7/hop.nvim",
+	opts = {
+		multi_windows = true,
+		keys = "htnsueoaidgcrlypmbkjvx",
+		uppercase_labels = true,
+	},
+	keys = {
+		{
+			"<leader>h",
+			function()
+				require("hop").hint_words()
+			end,
+			mode = { "n", "x", "o" },
+		},
+	},
+}
