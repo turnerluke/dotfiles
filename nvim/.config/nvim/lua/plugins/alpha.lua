9f2a487c86114608c91a4b10c7034661775b0944
turnerluke <turnermluke@gmail.com>
2025-01-25 09:37:23 -0600

    feat: minimalized alpha landing page

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -10,7 +10,7 @@ return {
 		local dashboard = require("alpha.themes.dashboard")
 
 		dashboard.section.header.val = {
-			[[                                                                       ]],
+			-- [[                                                                       ]],
 			[[                                                                     ]],
 			[[       ████ ██████           █████      ██                     ]],
 			[[      ███████████             █████                             ]],
@@ -19,20 +19,20 @@ return {
 			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
 			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
 			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			[[                                                                       ]],
+			-- [[                                                                       ]],
 		}
 
 		dashboard.section.buttons.val = {
-                dashboard.button("j", "󰈚   Restore Session", ":SessionRestore<cr>"),
-                dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
-                dashboard.button("f", "   Find file", ":cd $HOME/dotfiles | Telescope find_files<CR>"),
-                dashboard.button("g", "󰱼   Find word", ":Telescope live_grep<CR>"),
-                dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
-                dashboard.button("c", "   Config", ":e $MYVIMRC <CR>"),
-                dashboard.button("m", "󱌣   Mason", ":Mason<CR>"),
-                dashboard.button("l", "󰒲   Lazy", ":Lazy<CR>"),
-                dashboard.button("u", "󰂖   Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
-                dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
+			dashboard.button("j", "󰈚   Restore Session", ":SessionRestore<cr>"),
+			dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("f", "   Find file", ":cd $HOME/dotfiles | Telescope find_files<CR>"),
+			dashboard.button("g", "󰱼   Find word", ":Telescope live_grep<CR>"),
+			dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+			-- dashboard.button("c", "   Config", ":e $MYVIMRC <CR>"),
+			dashboard.button("m", "󱌣   Mason", ":Mason<CR>"),
+			dashboard.button("l", "󰒲   Lazy", ":Lazy<CR>"),
+			-- dashboard.button("u", "󰂖   Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
+			dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
 		}
 		_Gopts = {
 			position = "center",
@@ -42,12 +42,12 @@ return {
 
 		local function footer()
 			return {
-                [[ ]],
-                [[ The computer scientist's main challenge is not to get confused ]], 
-                [[ by the complexities of his own making. ]],
-                [[ ]],
-                [[ -Edsger W. Dijkstra ]]
-            }
+				-- [[ ]],
+				[[ The computer scientist's main challenge is not to get confused ]],
+				[[ by the complexities of his own making. ]],
+				[[ ]],
+				[[ -Edsger W. Dijkstra ]],
+			}
 		end
 
 		dashboard.section.footer.val = footer()
