40528a8a172cd48ed846efb1157700ade1e91444
turnerluke <turnermluke@gmail.com>
2025-02-02 11:24:04 -0600

    feat: add sqlfluff, move format keymap to liter

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -13,25 +13,27 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.completion.spell,
 				null_ls.builtins.diagnostics.mypy,
+				null_ls.builtins.formatting.sqlfluff,
 				-- null_ls.builtins.diagnostics.ruff,
 				-- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
 			},
 			on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({
-							group = augroup,
-							buffer = bufnr,
-						})
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								vim.lsp.buf.format({ bufnr = bufnr })
-							end,
-						})
-					end
-				end,
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({
+						group = augroup,
+						buffer = bufnr,
+					})
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
 		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		-- <leader>f to format in linter.lua
+		-- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
 	end,
 }
