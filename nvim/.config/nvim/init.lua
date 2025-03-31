0296acb0ea18a35c2064412411e7f1ffd8fd0659
turnerluke <turnermluke@gmail.com>
2025-03-31 13:36:09 -0500

    fix: move and reformat keymaps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,3 @@
-
 --- Initialize lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -16,3 +15,28 @@ vim.opt.rtp:prepend(lazypath)
 require("vim-options")
 require("lazy").setup("plugins")
 
+-- Yank to EOL
+vim.keymap.set("n", "Y", "y$", { desc = "Yank to EOL" })
+
+-- Run @q macro
+vim.keymap.set("n", "Q", "@q", { desc = "Run @q macro" })
+
+-- Auto recenter when searching
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+
+-- Delete/Change/Replace without Yanking
+vim.keymap.set("x", "<leader>p", '"_dP', { desc = "Replace without yanking" })
+
+vim.keymap.set("n", "<leader>d", '"_d', { desc = "Delete without yanking" })
+vim.keymap.set("n", "<leader>D", '"_D', { desc = "Delete to EOL without yanking" })
+
+vim.keymap.set("n", "<leader>c", '"_c', { desc = "Change without yanking" })
+vim.keymap.set("n", "<leader>C", '"_C', { desc = "Change to EOL without yanking" })
+
+-- Yank/Put with OS Clipboard
+vim.keymap.set("", "<leader>y", '"+y', { desc = "Yank to clipboard" })
+vim.keymap.set("", "<leader>Y", '"+y$', { desc = "Yank to EOL to clipboard" })
+
+vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste after cursor from clipboard" })
+vim.keymap.set("n", "<leader>P", '"+P', { desc = "Paste before cursor from clipboard" })
