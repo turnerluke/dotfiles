18bd01b96060fd96a5cb50ceeeee46db59c4dd96
turnerluke <turnermluke@gmail.com>
2025-05-04 10:57:12 -0500

    feat: initial release of autopairs

---
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,14 @@
+--[[
+
+nvim-autopairs
+
+
+]]
+
+return {
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	config = true,
+	-- use opts = {} for passing setup options
+	-- this is equivalent to setup({}) function
+}
