9393991ed5416cbb3697268351f5ecd66e15c5e1
Turner Luke <turner.luke@riospartners.com>
2025-05-23 23:52:59 -0400

    fix: remove buttons

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -6,6 +6,13 @@ M.base46 = {
 
 M.ui = {
 	statusline = { theme = "default" },
+
+	tabufline = {
+		enabled = true,
+		lazyload = true,
+		order = { "treeOffset", "buffers", "tabs" },
+		bufwidth = 21,
+	},
 }
 
 return M
